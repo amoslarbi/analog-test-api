3dd11050d792086c30b9bf2f32e8c478b6e7d05b
Khay <larbiamos18@gmail.com>
2020-11-19 17:08:23 +0000

    soft work

--- app/controllers/auth.js
+++ app/controllers/auth.js
@@ -21,9 +21,42 @@ const routes = (app) => {
   });
 
   // Register and Send activation email
-  app.post(PREFIX+'/register', async (req, res) => {
+  // app.post(PREFIX+'/register', async (req, res) => {
     
+  // });
+
+  app.post('/register', async function(req, res){
+    let fullName = trim(req.body.fullName);
+    let country = trim(req.body.country);
+    let email = trim(req.body.email);
+    let password = trim(req.body.password);
+    let passwordHash = hashPassword(password);
+
+    let signUpQuery = "INSERT INTO users (uuid, `fullName`, `email`, `country`, `password`, `hash`, `createdAt`, `accountType`, `accountStatus`) VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+    let signUp;
+    try{
+      [signUp] = await db.execute(signUpQuery, [uuid, fullName, email, country, passwordHash, hash, "NOW()", "Pending"]);
+    }catch(error){
+      console.log('SQL-Error: '+error);
+      return res.status(500).json({
+        status: 500,
+        message: 'Could not connect to server'
+      });
+    }
+
+    if (signUp) {
+
+    }
+    else{
+      return res.status(401).json({
+        status: 401,
+        message: "Error, unable to execute query"
+      });
+    }
+
   });
+
+
 }
 
 module.exports = {
