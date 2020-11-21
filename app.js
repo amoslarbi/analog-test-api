e7607220b12eb34451d9985a357a22c6ad174bb8
REk <reked@live.com>
2020-11-21 15:43:05 +0000

    app.js updated

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ const app = express();
 
 const middleware = require('./app/middleware/middleware');
 const apiRoutes = require('./routes');
-app.use(bodyParser.urlencoded({ extended: true }));
+
+// parsing the request body
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb'}));
+// json body
+app.use(express.json());
+
 middleware.setHeaders(app); // middleware to initialize headers
 middleware.initCookie(app); // middleware to initialize cookies
 const sessionChecker = middleware.sessionChecker;
