c0ce4799bcfb829b140d887b8580b326fa6bc839
REk <reked@live.com>
2020-12-10 21:11:05 +0000

    updated save election information

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-
+// const cors = require("cors");
 const middleware = require('./app/middleware/middleware');
 const apiRoutes = require('./routes');
 
+// app.use(cors());
 // parsing the request body
 app.use(bodyParser.urlencoded({ extended: true, limit: '100mb'}));
 // json body
--- app/controllers/client.js
+++ app/controllers/client.js
@@ -1,8 +1,7 @@
 const db = require('../database/connection');
 const jwt = require('jsonwebtoken');
 const { v4: uuidv4, v5: uuidv5 } = require('uuid');
-const cors = require("cors");
-const upload = require("../utilities/imageUpload");
+const upload = require("../utilities/image-upload");
 const singleUpload = upload.single("ballotIcon");
 const {
   trim,
@@ -469,7 +468,7 @@ const routes = (app, sessionChecker) => {
 
       // verify election UUID start
       app.post(PREFIX+'/verify-election-uuid', sessionChecker, async (req, res) => {
-      
+        console.dir(req.body)
         const uuid = req.uuid;
         let electionUUID = trim(req.body.electionUUID);
     
@@ -510,10 +509,10 @@ const routes = (app, sessionChecker) => {
   
       // insert information start
       app.post(PREFIX+'/information', sessionChecker, async (req, res) => {
-        app.use(cors());
+        // console.log(req);
         
+
         const uuid = req.uuid;
-        console.log(req);
         let ballotIcon;
 
         singleUpload(req, res, function (err) {
@@ -528,128 +527,135 @@ const routes = (app, sessionChecker) => {
             });
           }
 
+          
+
           ballotIcon = { profilePicture: req.file.location };
-      
-        });
-
-        let electionUUID = req.body.electionUUID;
-        console.log(electionUUID);
-        let name = req.body.name;
-        let organization_name = req.body.organization_name;
-        let duration = req.body.duration;
-        let declaration = req.body.declaration;
-        let declarationStatus;
-        if(declaration == "show"){
-          declarationStatus = 1
-        }
-        else{
-          declarationStatus = 0
-        }
-
-        // let getDuration = [];
-        // getDuration = duration;
-        // let start_time = getDuration[0];
-        // let end_time = getDuration[1];
-
-        // let durationKyiv = duration.split(' ');
-        // // let start_time_day = durationKyiv[0].split(',');
-        // let start_time_month = durationKyiv[1].split(',');
-        // let start_time_day = durationKyiv[2].split(',');
-        // let start_time_year = durationKyiv[3].split(',');
-        // let start_time_time = durationKyiv[4].split(',');
-        // let start_time = start_time_month + " " + start_time_day + " " + start_time_year + " " + start_time_time;
-
-        // let end_time_month = durationKyiv[6].split(',');
-        // let end_time_day = durationKyiv[7].split(',');
-        // let end_time_year = durationKyiv[8].split(',');
-        // let end_time_time = durationKyiv[9].split(',');
-        // let end_time = end_time_month + " " + end_time_day + " " + end_time_year + " " + end_time_time;
-
-        let errorInfo = {}
-        let errorCount = 0;
-
-        if(name.length === 0){
-          errorCount++;
-          errorInfo.name = "Enter election name";
-        }
-    
-        if(organization_name.length === 0){
-          errorCount++;
-          errorInfo.organization_name = "Enter organization / group name";
-        }
-
-        if(duration.length === 0){
-          errorCount++;
-          errorInfo.duration = "Enter duration";
-        }
-
-        let checkInformationElectionUUIDQuery = "SELECT `id`, `name`, `organization_name` FROM elections WHERE `election_uuid` = ? AND `created_by` = ?";
-        let checkInformationElectionUUID;
-        try{
-          [checkInformationElectionUUID] = await db.execute(checkInformationElectionUUIDQuery, [ electionUUID, uuid ]);
-        }catch(error){
-          console.log('SQL-Error: '+error);
-          sendMessageToTelegram('bug', 'SQL-Error: '+error+'--'+checkInformationElectionUUIDQuery);
-          return res.status(500).json({
-            status: 500,
-            message: 'Could not connect to server'
-          });
-        }
-
-        if (checkInformationElectionUUID.length === 0) {
+          console.dir(ballotIcon);
           return res.status(400).json({
             status: 400,
             message: "Invalid election ID"
           });
-        }
-    
-        // let informationElectionNameQuery = "SELECT * FROM `elections` WHERE (`name` = ? AND `organization_name` = ?) AND `created_by` = ?  ";
-        // let checkInformationElectionNameQuery;
-        // try{
-        //   [checkInformationElectionNameQuery] = await db.execute(informationElectionNameQuery, [ name, organization_name, uuid ]);
-        // }catch(error){
-        //   console.log('SQL-Error: '+error);
-        //   sendMessageToTelegram('bug', 'SQL-Error: '+error+'--'+informationElectionNameQuery);
-        //   return res.status(500).json({
-        //     status: 500,
-        //     message: 'Could not connect to server'
-        //   });
-        // }
-
-        let errorMessage = "Error: Sorry, failed to create election";
-    
-        // if (checkInformationElectionNameQuery.length === 1) {
-        //   errorCount++;
-        //   errorMessage = "You already have an election with the same name and organization/group name.";
-        // }
-
-        if(errorCount > 0){
-          return res.status(400).json({
-            status: 400,
-            message: errorMessage,
-            errors: errorInfo
-          });
-        }
-    
-        let updateInformationElectionQuery = "UPDATE elections SET `icon` = ?, `name` = ?, `organization_name` = ?, `start_time` = ?, `end_time` = ?, `show_result` = ?";
-        let checkUpdateInformationElectionQuery;
-        try{
-          [checkUpdateInformationElectionQuery] = await db.execute(updateInformationElectionQuery, [ ballotIcon, name, organization_name, start_time, end_time, declarationStatus ]);
-        }catch(error){
-          console.log('SQL-Error: '+error);
-          sendMessageToTelegram('bug', 'SQL-Error: '+error+'--'+updateInformationElectionQuery);
-          return res.status(500).json({
-            status: 500,
-            message: 'Could not connect to server'
-          });
-        }
-    
-        let alertMessage = `ELECTION (Draft):\n Election Name: ${name} \n Organization Name: ${organization_name}.`
-        sendMessageToTelegram('alert', alertMessage);
-        return res.status(200).json({
-          status: 200,
-          message: "worked",
+      
         });
+
+      //   let electionUUID = req.body.electionUUID;
+      //   console.log(electionUUID);
+      //   let name = req.body.name;
+      //   let organization_name = req.body.organization_name;
+      //   let duration = req.body.duration;
+      //   let declaration = req.body.declaration;
+      //   let declarationStatus;
+      //   if(declaration == "show"){
+      //     declarationStatus = 1
+      //   }
+      //   else{
+      //     declarationStatus = 0
+      //   }
+
+      //   // let getDuration = [];
+      //   // getDuration = duration;
+      //   // let start_time = getDuration[0];
+      //   // let end_time = getDuration[1];
+
+      //   // let durationKyiv = duration.split(' ');
+      //   // // let start_time_day = durationKyiv[0].split(',');
+      //   // let start_time_month = durationKyiv[1].split(',');
+      //   // let start_time_day = durationKyiv[2].split(',');
+      //   // let start_time_year = durationKyiv[3].split(',');
+      //   // let start_time_time = durationKyiv[4].split(',');
+      //   // let start_time = start_time_month + " " + start_time_day + " " + start_time_year + " " + start_time_time;
+
+      //   // let end_time_month = durationKyiv[6].split(',');
+      //   // let end_time_day = durationKyiv[7].split(',');
+      //   // let end_time_year = durationKyiv[8].split(',');
+      //   // let end_time_time = durationKyiv[9].split(',');
+      //   // let end_time = end_time_month + " " + end_time_day + " " + end_time_year + " " + end_time_time;
+
+      //   let errorInfo = {}
+      //   let errorCount = 0;
+
+      //   if(name.length === 0){
+      //     errorCount++;
+      //     errorInfo.name = "Enter election name";
+      //   }
+    
+      //   if(organization_name.length === 0){
+      //     errorCount++;
+      //     errorInfo.organization_name = "Enter organization / group name";
+      //   }
+
+      //   if(duration.length === 0){
+      //     errorCount++;
+      //     errorInfo.duration = "Enter duration";
+      //   }
+
+      //   let checkInformationElectionUUIDQuery = "SELECT `id`, `name`, `organization_name` FROM elections WHERE `election_uuid` = ? AND `created_by` = ?";
+      //   let checkInformationElectionUUID;
+      //   try{
+      //     [checkInformationElectionUUID] = await db.execute(checkInformationElectionUUIDQuery, [ electionUUID, uuid ]);
+      //   }catch(error){
+      //     console.log('SQL-Error: '+error);
+      //     sendMessageToTelegram('bug', 'SQL-Error: '+error+'--'+checkInformationElectionUUIDQuery);
+      //     return res.status(500).json({
+      //       status: 500,
+      //       message: 'Could not connect to server'
+      //     });
+      //   }
+
+      //   if (checkInformationElectionUUID.length === 0) {
+      //     return res.status(400).json({
+      //       status: 400,
+      //       message: "Invalid election ID"
+      //     });
+      //   }
+    
+      //   // let informationElectionNameQuery = "SELECT * FROM `elections` WHERE (`name` = ? AND `organization_name` = ?) AND `created_by` = ?  ";
+      //   // let checkInformationElectionNameQuery;
+      //   // try{
+      //   //   [checkInformationElectionNameQuery] = await db.execute(informationElectionNameQuery, [ name, organization_name, uuid ]);
+      //   // }catch(error){
+      //   //   console.log('SQL-Error: '+error);
+      //   //   sendMessageToTelegram('bug', 'SQL-Error: '+error+'--'+informationElectionNameQuery);
+      //   //   return res.status(500).json({
+      //   //     status: 500,
+      //   //     message: 'Could not connect to server'
+      //   //   });
+      //   // }
+
+      //   let errorMessage = "Error: Sorry, failed to create election";
+    
+      //   // if (checkInformationElectionNameQuery.length === 1) {
+      //   //   errorCount++;
+      //   //   errorMessage = "You already have an election with the same name and organization/group name.";
+      //   // }
+
+      //   if(errorCount > 0){
+      //     return res.status(400).json({
+      //       status: 400,
+      //       message: errorMessage,
+      //       errors: errorInfo
+      //     });
+      //   }
+    
+      //   let updateInformationElectionQuery = "UPDATE elections SET `icon` = ?, `name` = ?, `organization_name` = ?, `start_time` = ?, `end_time` = ?, `show_result` = ?";
+      //   let checkUpdateInformationElectionQuery;
+      //   try{
+      //     [checkUpdateInformationElectionQuery] = await db.execute(updateInformationElectionQuery, [ ballotIcon, name, organization_name, start_time, end_time, declarationStatus ]);
+      //   }catch(error){
+      //     console.log('SQL-Error: '+error);
+      //     sendMessageToTelegram('bug', 'SQL-Error: '+error+'--'+updateInformationElectionQuery);
+      //     return res.status(500).json({
+      //       status: 500,
+      //       message: 'Could not connect to server'
+      //     });
+      //   }
+    
+      //   let alertMessage = `ELECTION (Draft):\n Election Name: ${name} \n Organization Name: ${organization_name}.`
+      //   sendMessageToTelegram('alert', alertMessage);
+      //   return res.status(200).json({
+      //     status: 200,
+      //     message: "worked",
+      //   });
 
       });
   
