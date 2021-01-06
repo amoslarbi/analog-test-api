5fe1a09f0e1340ee41705d782af3393502c3e0f1
Edwin Roger <reked@live.com>
2021-01-06 15:50:13 +0000

    upload voters csv api endpoint updated

--- routes.js
+++ routes.js
@@ -11,22 +11,6 @@ const routes = (app, sessionChecker) => {
     res.end("ping-pong");
   });
 
-  app.get('/upload-voters-csv', async (req, res) => {
-    getCSV('app/data.csv')
-    .then(rows => {
-      console.log(rows)
-      if(rows[0].Name === undefined){
-        console.log('Invalid CSV')
-      }
-      // let info = 
-      res.setHeader("Content-Type", "text/plain");
-      res.end("Data:"+JSON.stringify(rows)+'<br><br>'+rows[1].Name);
-    }).catch((error) => {
-      res.setHeader("Content-Type", "text/plain");
-      res.end("error:"+error);
-    });
-  })
-
   authentication.routes(app); // authentication routes
   client.routes(app, sessionChecker); // client routes
   voter.routes(app, sessionChecker); // voter routes
