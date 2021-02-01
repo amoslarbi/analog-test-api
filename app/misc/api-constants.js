b228d6903683a2edb20e7c61a20f317b3932e3f3
Khay <larbiamos18@gmail.com>
2021-02-01 17:29:33 +0000

    Bug Fixes

--- app/misc/api-constants.js
+++ app/misc/api-constants.js
@@ -1,13 +1,6 @@
 const API_CONSTANTS = {
   Constants: {
-    VOTE_COST: {
-      FREE_VOTERS_LIMIT: 25,
-      GHANA_COST_PER_VOTE: 0.50,
-      NIGERIA_COST_PER_VOTE: 50,
-      COST_PER_VOTE: 0.50,
-    },
-    OBALLOT_URL:  process.env.API_MODE === 'production' ? "https://oballot.com" : "http://localhost:7070",
-    CLIENT_APP_URL:  process.env.API_MODE === 'production' ? "https://app.oballot.com" : "http://localhost:3000",
+    OBALLOT_URL:  process.env.API_MODE === 'production' ? "https://analogapi.oballot.com" : "http://localhost:7770",
   }
 }
 
