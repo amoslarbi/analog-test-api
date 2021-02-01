facd635bf75d6a36ae650d36f152730638f286eb
Khay <larbiamos18@gmail.com>
2021-02-01 17:35:19 +0000

    Changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const http = require('http');
 const app = require('./app');
-const port = process.env.PORT || 5050;
+const port = process.env.PORT || 7770;
 const server = http.createServer(app);
 
 server.listen(port);
